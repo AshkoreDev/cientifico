6eee52dd19ba1e2d6c84529b9c706352a9966071
Aracely Alba <aracelyalba8@gmail.com>
2022-03-01 19:22:44 +0100

    fixing footer

--- src/templates/footer.js
+++ src/templates/footer.js
@@ -2,14 +2,14 @@ const footer = function() {
   const view = `
     <section class="footer__container">
       <article class="footer__info">
-        <a href="https://www.linkedin.com/in/AshkoreDev/" target="_blank" title="Linkedin" class="footer__info--item">
-          <img src="./images/icon-linkedin.svg" class="footer__info--icon" alt="Logo Linkedin"/>
+        <a title="Linkedin" href="https://www.linkedin.com/in/AshkoreDev/" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" class="footer__info--icon" alt="Logo Linkedin"/>
         </a>
-        <a href="https://github.com/AshkoreDev" target="_blank" title="GitHub" class="footer__info--item">
-          <img src="./images/icon-github.svg" class="footer__info--icon" alt="Logo GitHub"/>
+        <a title="GitHub" href="https://github.com/AshkoreDev" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-filled/50/FFFFFF/github.png" class="footer__info--icon" alt="Logo GitHub"/>
         </a>
-        <a href="https://twitter.com/AshkoreDev" target="_blank" title="Twitter" class="footer__info--item">
-          <img src="./images/icon-twitter.svg" class="footer__info--icon" alt="Logo Twitter"/>
+        <a title="Twitter" href="https://twitter.com/AshkoreDev" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/twitter--v1.png" class="footer__info--icon" alt="Logo Twitter"/>
         </a>
       </article>
       <p class="footer__info">Aracely Alba @AshkoreDev | &copy; 2022</p>

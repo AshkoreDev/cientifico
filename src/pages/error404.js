8e55fe40b8883d1fb6619ff94d14703333a08f61
Aracely Alba <aracelyalba8@gmail.com>
2022-03-01 19:55:49 +0100

    add error 404 page

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -1,7 +1,10 @@
 const error404 = function() {
   const view = `
     <section class="error404">
-      <h2>Error 404</h2>
+    <h2 class="error404__title">Error 404</h2>
+      <figure>
+        <img src="./../images/imagen-404.png" alt="Error 404 image" width="300" height='374'/>
+      </figure>
     </section>
   `;
 

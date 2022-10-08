9ec3a930b08225ce7167a0cd9a1ed767935eb9c7
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-10-08 21:19:07 +0200

    Added footer template

---
+++ src/templates/footer.js
@@ -0,0 +1,23 @@
+const footer = function() {
+  const view = `
+    <section class="footer__container">
+      <article class="footer__info">
+        <a title="Linkedin" href="https://www.linkedin.com/in/AshkoreDev/" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" class="footer__info--icon" alt="Logo Linkedin"/>
+        </a>
+        <a title="GitHub" href="https://github.com/AshkoreDev" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-filled/50/FFFFFF/github.png" class="footer__info--icon" alt="Logo GitHub"/>
+        </a>
+        <a title="Twitter" href="https://twitter.com/AshkoreDev" target="_blank" rel="noopener" class="footer__info--item">
+          <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/twitter--v1.png" class="footer__info--icon" alt="Logo Twitter"/>
+        </a>
+      </article>
+      <p class="footer__info">Aracely Alba @AshkoreDev | &copy; 2022</p>
+    </section>
+  `;
+  return view;
+}
+
+
+
+export default footer;
